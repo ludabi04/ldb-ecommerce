bee1cf5e05d5a3c2cad9feadd9c4611bad570dee
Lucas <ludabi@live.com>
2023-06-18 00:37:16 -0300

    agregando firestore

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,36 +1,35 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import productos from '../productos.json'
 import ItemList from "../ItemList/ItemList";
 import { useParams } from "react-router-dom";
 import './ItemListContainer.css'
 import Loading from "../Loading/Loading";
+import { getFirestore, query, where, collection, getDocs } from "firebase/firestore"
 
 const ItemListContainer = () => {
     const [items, setItems] = useState([]);
     const { id } = useParams();
-    const [loading, setLoading ] = useState(true)
+    const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const promesa = new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(id ? productos.filter(item => item.categoria === id) : productos);;
-            }, 2000);
-        });
-        
-        promesa.then(data => {
-            setItems(data);
-            setLoading(false)
-        });
-    }, [id]);
+      useEffect(() => {
+          const db = getFirestore();
+          const itemsCollection = collection(db, "items");
+          const q = id ? query(itemsCollection, where("categoria", "==", id)) : itemsCollection;
+
+          getDocs(q).then(resultado => {
+              setItems(resultado.docs.map((producto) => ({ id: producto.id, ...producto.data() })));
+              setLoading(false)
+          });
+      }, [id]);
+
 
     return (
-        
-            <div className="listContainer">
-            {loading ? <Loading/> :
+
+        <div className="listContainer">
+            { loading ? <Loading /> :
                 <ItemList productos={ items } /> }
-            </div>
-        
+        </div>
+
     )
 }
 
